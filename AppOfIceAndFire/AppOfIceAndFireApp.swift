3f1fd963e974d9a0c265234b587138dedb4e7839
Dali Andjelkovic <dalibor.andjelkovic@cegeka.de>
2022-02-21 08:09:21 +0100

    Initial Commit

---
+++ AppOfIceAndFire/AppOfIceAndFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOfIceAndFireApp.swift
+//  AppOfIceAndFire
+//
+//  Created by Dalibor Andjelkovic on 21.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AppOfIceAndFireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
